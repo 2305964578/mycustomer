92121e94bb4109593ece3150a20db4bb574e27c6
23059 <https://github.com/2305964578/hiworld>
2020-03-17 16:20:28 +0800

    创建mycustomer

---
+++ src/main/java/cn/google/mycustomer/MycustomerApplication.java
@@ -0,0 +1,13 @@
+package cn.google.mycustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/google/mycustomer/MycustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.google.mycustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
